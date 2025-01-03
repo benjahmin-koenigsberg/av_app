35d80d5971f552b74889e897a14a28ca0e251936
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2025-01-03 00:16:23 -0500

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,40 @@
+
+
+//Question 1 English
+const q1English = new Audio('https://res.cloudinary.com/dhc1h071n/video/upload/v1735880529/Sounds/q1-english_nlb2ge.mp3'); // Replace 'your_audio_file.mp3' with the actual path
+const q1_english = document.getElementById('q1_english')
+q1_english.addEventListener('click', playQ1English)
+
+function playQ1English() { 
+  q1English.play(); 
+} 
+
+//Question 1 Spanish
+const q1Spanish = new Audio('https://res.cloudinary.com/dhc1h071n/video/upload/v1735880530/Sounds/q1-spanish_zkmwkt.mp3'); // Replace 'your_audio_file.mp3' with the actual path
+const q1_spanish = document.getElementById('q1_spanish')
+q1_spanish.addEventListener('click', playQ1Spanish)
+
+
+function playQ1Spanish() { 
+  q1Spanish.play(); 
+} 
+
+//Question 1 Russian
+const q1Russian = new Audio('https://res.cloudinary.com/dhc1h071n/video/upload/v1735880529/Sounds/q1-russian_rif7ej.mp3'); // Replace 'your_audio_file.mp3' with the actual path
+const q1_russian = document.getElementById('q1_russian')
+q1_russian.addEventListener('click', playQ1Russian)
+
+
+function playQ1Russian() { 
+  q1Russian.play(); 
+} 
+
+//Question 1 Chinesse
+const q1Chinesse = new Audio('https://res.cloudinary.com/dhc1h071n/video/upload/v1735880530/Sounds/q1-chinese_kbqrww.mp3'); // Replace 'your_audio_file.mp3' with the actual path
+const q1_chinesse = document.getElementById('q1_chinesse')
+q1_chinesse.addEventListener('click', playQ1Chinesse)
+
+
+function playQ1Chinesse() { 
+  q1Chinesse.play(); 
+} 
