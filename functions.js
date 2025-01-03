4f34c4e40e0409eff248f333d7113a0be0c5c3a1
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2025-01-02 19:29:09 -0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,99 @@
+const thumbsUp = document.getElementById("thumbs_up");
+const thumbsDown = document.getElementById("thumbs_down");
+const upCounter = document.getElementById("up_count");
+const downCounter = document.getElementById("down_count");
+const sum = document.getElementById("sum");
+const notes = document.getElementById("notes");
+
+const datetime = new Date();
+const date = datetime.toDateString(); 
+const seconds = datetime.getSeconds()
+
+let countObj = {
+    up: upCounter,
+    down: downCounter,
+    total: upCounter + downCounter,
+    notes: notes,
+  };
+
+
+function resetCount() {
+    //localStorage.clear();
+    countObj = {}
+    localStorage.setItem("count", JSON.stringify(countObj));
+    upCounter.textContent = 0;
+    downCounter.textContent = 0;
+    sum.textContent = 0;
+    notes.value = "";
+  }
+
+
+ function saveNotes() {
+  
+    countObj.up = upCounter.textContent,
+    countObj.down =downCounter.textContent,
+    countObj.total = sum.textContent,
+    countObj.notes = notes.value;
+  
+    localStorage.setItem("count", JSON.stringify(countObj));
+  }
+
+
+ function addOneUp() {
+   
+    upCounter.textContent++;
+    sum.textContent++;
+
+    countObj.up = upCounter.textContent,
+    countObj.down =downCounter.textContent,
+    countObj.total = sum.textContent,
+    countObj.notes = notes.value;
+  
+    localStorage.setItem("count", JSON.stringify(countObj));
+  }
+  
+ function addOneDown() {
+    downCounter.textContent++;
+    sum.textContent++;
+  
+    countObj.up = upCounter.textContent,
+    countObj.down =downCounter.textContent,
+    countObj.total = sum.textContent,
+    countObj.notes = notes.value;
+  
+    localStorage.setItem("count", JSON.stringify(countObj));
+  }
+
+  function generateUniqueId() {
+    const timestamp = Date.now().toString(36);
+    const random = Math.random().toString(36).substr(2, 9);
+    return timestamp + random;
+  }
+
+  function storeDaysData(){
+    
+    let daysData = {}
+    daysData.up = upCounter.textContent,
+    daysData.down =downCounter.textContent,
+    daysData.total = sum.textContent,
+    daysData.notes = notes.value;
+    daysData.date = date;
+
+    localStorage.setItem(generateUniqueId(), JSON.stringify(daysData));
+   
+  }
+
+  let archive = [];
+
+  function getAllStorage() {
+   
+    for (let i = 0; i<localStorage.length; i++) {
+        archive[i] = localStorage.getItem(localStorage.key(i));
+    }
+    return archive;
+}
+
+getAllStorage()
+console.log(archive)
+
+ export {addOneDown, addOneUp, resetCount, saveNotes, storeDaysData, getAllStorage};
--- script.js
+++ script.js
@@ -1,84 +1,43 @@
+import { addOneDown, addOneUp, saveNotes, resetCount, storeDaysData, getAllStorage } from "./functions.js";
+
 const thumbsUp = document.getElementById("thumbs_up");
 const thumbsDown = document.getElementById("thumbs_down");
-let upCounter = document.getElementById("up_count");
-let downCounter = document.getElementById("down_count");
-let sum = document.getElementById("sum");
+const upCounter = document.getElementById("up_count");
+const downCounter = document.getElementById("down_count");
+const sum = document.getElementById("sum");
 const reset = document.getElementById("reset");
-let notes = document.getElementById("notes");
+const notes = document.getElementById("notes");
+const save = document.getElementById('save')
 
 const datetime = new Date();
 const date = datetime.toDateString();
-// const date = datetime.getDate()
-//const date = datetime.toLocaleDateString();
-// Insert date and time into HTML
 document.getElementById("datetime").innerText = date;
 
 reset.addEventListener("click", resetCount);
 thumbsUp.addEventListener("click", addOneUp);
 thumbsDown.addEventListener("click", addOneDown);
 notes.addEventListener("keydown", saveNotes);
+save.addEventListener('click', storeDaysData)
 
-function resetCount() {
-  localStorage.clear();
-  upCounter.textContent = 0;
-  downCounter.textContent = 0;
-  sum.textContent = 0;
-  notes.value = "";
-}
-
-function saveNotes() {
-  let countObj = {
-    up: upCounter.textContent,
-    down: downCounter.textContent,
-    total: sum.textContent,
-    notes: notes.value,
-  };
-
-  localStorage.setItem("count", JSON.stringify(countObj));
-}
 
 let countObj = {
-  up: upCounter,
-  down: downCounter,
-  total: upCounter + downCounter,
-  notes: notes,
+  up: upCounter.textContent,
+  down: downCounter.textContent,
+ total: upCounter.textContent + downCounter.textContent,
+  notes: notes.value,
 };
 
 const storedCount = localStorage.getItem("count");
 countObj = JSON.parse(storedCount);
+console.log(countObj)
+
 
 upCounter.textContent = countObj.up;
 downCounter.textContent = countObj.down;
 sum.textContent = countObj.total;
 notes.value = countObj.notes;
-console.log(notes);
 
-function addOneUp() {
-  upCounter.textContent++;
-  sum.textContent++;
-  // Save the object to localStorage
-  let countObj = {
-    up: upCounter.textContent,
-    down: downCounter.textContent,
-    total: sum.textContent,
-    notes: notes.value,
-  };
 
-  localStorage.setItem("count", JSON.stringify(countObj));
-}
-
-function addOneDown() {
-  downCounter.textContent++;
-  sum.textContent++;
-  let countObj = {
-    up: upCounter.textContent,
-    down: downCounter.textContent,
-    total: sum.textContent,
-    notes: notes.value,
-  };
-
-  localStorage.setItem("count", JSON.stringify(countObj));
-}
 
 const myModal = document.getElementById("myModal");
 const myInput = document.getElementById("myInput");
